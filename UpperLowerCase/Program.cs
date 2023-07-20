f4936798b5933a37a2559aa3c149ee7bf753f54c
Usama Iqbal <uiqbal@cedrus.com>
2023-07-20 17:14:43 +0500

    feat: This Program Converts given string into lower/upper case

---
+++ UpperLowerCase/Program.cs
@@ -0,0 +1,26 @@
+﻿
+public class program
+{
+public static void Main(string[] args)
+    {
+        Console.Write("Please Give the string value : ");
+        string str;
+       str=(Console.ReadLine());
+
+
+
+        Console.Write("After conversion, the string is : ");
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            char ch = str[i];
+            if (char.IsLower(ch))
+                Console.Write(char.ToUpper(ch)); 
+            else
+                Console.Write(char.ToLower(ch)); 
+        }
+
+    
+
+    }
+}
